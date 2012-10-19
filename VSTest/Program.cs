273f2d58d282186c2f0589774b663462c584cc45
Justin Swartsel <justinswartsel@gmail.com>
2012-10-19 13:22:30 -0400

    added method

--- VSTest/Program.cs
+++ VSTest/Program.cs
@@ -12,5 +12,14 @@ namespace VSTest
             Console.WriteLine("hello world");
             Console.ReadKey();
         }
+
+        private void JustinsExcellentMethod()
+        {
+            Console.WriteLine("Look how excellent this is...");
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
