9144429f4473fe2f5f8ada191c687fb29340a915
Justin Swartsel <justinswartsel@gmail.com>
2012-10-19 13:16:43 -0400

    added project

---
+++ VSTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
